15190025514de58e36f3fb9c3d634c9e5eb31661
Parbhat Puri <parbhatpuri17@gmail.com>
2014-10-31 12:36:44 +0530

    Basic View now returns minimal HTMl

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -9,3 +10,10 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
