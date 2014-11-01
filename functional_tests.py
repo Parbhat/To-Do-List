91ebd7d4ec2efcf5b1a97b8e4519aef28981deb4
Parbhat Puri <parbhatpuri17@gmail.com>
2014-11-02 01:45:37 +0530

    Added context variable to template

--- functional_tests.py
+++ functional_tests.py
@@ -31,18 +31,20 @@ class NewVisitorTest(unittest.TestCase):
 
 		#She types "Buy peacock feathers" into a text box (Edith's
 		# hobby is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys('Use peacock feathers to make a fly')
 
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
+		#import time
+		#time.sleep(10)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-				any(row.text == '1: Buy peacock feathers' for row in rows),
-				"New to-do item did not appear in table"
-			)
-
+		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly ',
+			[row.text for row in rows]
+		)
 		# There is still a text box inviting her to add another item.She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		self.fail('Finish the test!')
