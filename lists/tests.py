598ee16a10e39c17e9f3e3c94dd8a32cfe077239
Parbhat Puri <parbhatpuri17@gmail.com>
2014-11-02 13:14:18 +0530

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -24,15 +24,35 @@ class HomePageTest(TestCase):
 		request.POST['item_text'] = 'A new list item'
 
 		response = home_page(request)
-		self.assertIn('A new list item',response.content.decode())
 
-		expected_html = render_to_string(
-			'home.html',
-			{'new_item_text': 'A new list item'}
-			)
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
 
-		self.assertEqual(response.content.decode(),expected_html)
+	def test_home_page_redirects_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
 
+		response = home_page(request)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_only_saves_items_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+
+	def test_home_page_displays_all_lists_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+	if request.method == 'POST':
+		new_item_text = request.POST['item_text']
+		Item.objects.create(text=new_item_text)
+		return redirect('/')
+	else:
+		new_item_text = ''
 
-	return render(request,'home.html',{
-			'new_item_text':request.POST.get('item_text',''),
-		})
+	items = Item.objects.all()
+	return render(request,'home.html',{'items': items})
