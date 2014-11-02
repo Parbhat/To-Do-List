98010a2cc17b69961f8c7152f97eb0a27d58bba6
Parbhat Puri <parbhatpuri17@gmail.com>
2014-11-02 13:15:38 +0530

    typo fixed in functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -49,7 +49,7 @@ class NewVisitorTest(unittest.TestCase):
 		# There is still a text box inviting her to add another item.She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys("Use peacock feathers to make fly")
+		inputbox.send_keys("Use peacock feathers to make a fly")
 		inputbox.send_keys(Keys.ENTER)
 		
 		# The page updates again, and now shows bth items on her list
