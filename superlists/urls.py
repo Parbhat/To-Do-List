1813d7283441d17060ea3b37b62ca310d44d574b
Parbhat Puri <parbhatpuri17@gmail.com>
2014-11-04 18:47:55 +0530

    Refactored Urls:created separate urls.py for lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,11 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list',
-     	name='view_list'
-     ),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^lists/', include('lists.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
